053778fc51709b7a771f17ea72ad7816e4fdcc27
ljf <sbfksmq@outlook.com>
2019-06-16 03:29:10 +0800

    debug

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -99,9 +99,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangoblog',
-        'USER': os.environ.get('DJANGO_MYSQL_USER'),
-        'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD'),
-        'HOST': os.environ.get('DJANGO_MYSQL_HOST'),
+        # 'USER': os.environ.get('DJANGO_MYSQL_USER'),
+        'USER': 'root',
+        # 'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD'),
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
         'PORT': 3306,
         'OPTIONS': {'charset': 'utf8mb4'},
     }
